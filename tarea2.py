f11dcbd8cc2fdadb43481c4e38e2f8fdb3af2ae6
agk7BOOST <147288332+agk7BOOST@users.noreply.github.com>
2024-03-12 22:54:04 -0300

    Update tarea2.py

--- tarea2.py
+++ tarea2.py
@@ -1,3 +1,16 @@
-# Hacer un algoritmo que le permita cargar un array de n elementos a eleccion del usuario
-# imprimir el array ingresado en orden 
-# ordenar el array en forma ascendente (de menor a mayor) e imprimir ese array ordenado
+arr=[]
+n= int(input("Ingresa el numero de elementos \n"))
+i=0 
+for i in range(int(n)):
+    x=int(input(f'ingresa el elemento nro {i+1} \n'))
+    arr.append(x)
+
+for i in range(n):
+    # Últimos i elementos ya están en su lugar correcto
+    for j in range(0, n-i-1):
+        # Intercambia si el elemento encontrado es mayor que el siguiente elemento
+        if arr[j] > arr[j+1]:
+            arr[j], arr[j+1] = arr[j+1], arr[j]
+
+for item in arr:
+    print(item)
